e87c5faf598ae558db5e2ff0ad9d6dab223b0dfe
Jameel Qureshi <jameelqureshi2013@gmail.com>
2020-07-17 19:29:15 +0500

    Camera Working

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject loginScreen;
+    public GameObject menuScreen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        loginScreen.SetActive(true);
+        menuScreen.SetActive(false);
+
+    #if UNITY_ANDROID
+        if (!Application.HasUserAuthorization(UserAuthorization.WebCam)) {
+            Application.RequestUserAuthorization(UserAuthorization.WebCam);
+        }
+    #endif
+
+    }
+
+    public void Login()
+    {
+        loginScreen.SetActive(false);
+        menuScreen.SetActive(true);
+    }
+
+    public void StartDay()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+}
